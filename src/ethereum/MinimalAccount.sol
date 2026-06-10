176b4c4c8f5aa1e13d457fd6047c85265a94e6d5
arinaitweallan <allan.bnb2@gmail.com>
2026-06-10 23:01:01 +0300

    minimal acc

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -7,13 +7,25 @@ import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import {MessageHashUtils} from "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
 import {ECDSA} from "lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
 contract MinimalAccount is IAccount, Ownable {
-    constructor() Ownable(msg.sender) {}
+    IEntryPoint public immutable ENTRY_POINT;
+
+    constructor(address _entryPoint) Ownable(msg.sender) {
+        ENTRY_POINT = IEntryPoint(_entryPoint);
+    }
+
+    /// modifiers
+    modifier requireFromEntryPoint() {
+        require((msg.sender == address(ENTRY_POINT)), "not from entry point");
+        _;
+    }
 
     /// @notice A signature is valid if it's the `MinimalAccount` owner
     function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
         external
+        requireFromEntryPoint
         returns (uint256 validationData)
     {
         validationData = _validateSignature(userOp, userOpHash);
@@ -40,8 +52,8 @@ contract MinimalAccount is IAccount, Ownable {
     /// @notice EntryPoint contract pays the gas funds
     function _payPrefund(uint256 missingAccountFunds) internal {
         if (missingAccountFunds > 0) {
-            (bool success, ) payable(msg.sender).call{value: missingAccountFunds gas: type(uint128).max}("");
-        require(success, "failed");
+            (bool success,) = payable(msg.sender).call{value: missingAccountFunds, gas: type(uint128).max}("");
+            require(success, "failed");
         }
     }
 }
